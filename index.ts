facd3db1718430cd13cc6edfa915d4ab4adfa308
Anwesh43 <anweshthecool0@gmail.com>
2020-11-03 17:33:12 +0530

    Adding Animator class

--- index.ts
+++ index.ts
@@ -66,3 +66,23 @@ class DrawingUtil {
         DrawingUtil.drawBallDropLine(context, x, y, scale)
     }
 }
+
+class Animator {
+
+    animated : boolean = false 
+    interval : number 
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true 
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false 
+            clearInterval(this.interval)
+        }
+    }
+}
